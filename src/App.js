1caab6baa1161cfb2364e894328c2cf744955ec9
Omrani Salim <omrani_salim@outlook.fr>
2018-09-22 22:58:01 +0200

    work with props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/person";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hi , I'm a React App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi , I'm a React App</h1>
+                <Person name="salim" age="25">Yes yes yes</Person>
+            </div>
+        );
+    }
 }
 
 export default App;
